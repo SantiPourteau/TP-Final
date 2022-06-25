a8e377e44e476e132ae60f4798cab0234bc70aab
maxividela2577 <mvidela@udesa.edu.ar>
2022-06-24 22:44:33 -0300

    add

--- src/mission_control_metalofono.py
+++ src/mission_control_metalofono.py
@@ -1,7 +1,20 @@
 import argparse
+from xylophone.client import XyloClient
+from xylophone.xylo import XyloNote
+from xylophone.server.server import XyloServer
+
 
 def interactuar(partitura,dispositivo):
-    pass
+    server=XyloServer()
+    server.start()
+    client=XyloClient()
+    notes=[]
+    with open(partitura,"r") as f:
+        for line in f:
+            notes.append(XyloNote(line[1],line[0],90))
+    client.load(notes)
+    client.play()
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='metalofon')
