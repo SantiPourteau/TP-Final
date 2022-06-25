bc13db2fa9a42da0d69e0798563fb10121e4b4dd
maxividela2577 <mvidela@udesa.edu.ar>
2022-06-25 09:47:46 -0300

    add

--- src/models/casing.py
+++ src/models/casing.py
@@ -12,14 +12,15 @@ class Casing():
     
     def case(self,note):
         time_array=np.arange(note.time(),note.get_time()+note.get_duration(),note.get_duration()/self.frequency)
-        freq_array=np.empty()
+        freq_list=[]
         for time in time_array:
             freq=self.A * self.instrument.get_harmonic_amplitude()[1] *sin(2*pi*note.get_frequency()*self.instrument.get_harmonic_amplitude()[0]*(time-note.get_time()))
-            freq_array.append(freq_array,freq)
+            freq_list.append(freq)
+        freq_array=np.array(freq_list)
         
         self.case=np.array(time_array,freq_array)
 
-    
+
     def get_wave(self):
         return Wave(self.case)
         
