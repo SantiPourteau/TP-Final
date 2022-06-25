c0561bae05a4b5d16fd05053780072cb18145bb4
maxividela2577 <mvidela@udesa.edu.ar>
2022-06-24 23:49:19 -0300

    add

--- src/models/casing.py
+++ src/models/casing.py
@@ -1,17 +1,30 @@
 from math import pi, sin
-
+import numpy as np
 from wave_obj import Wave
 
 
 class Casing():
-    def __init__(self,instrument,A):
-        self.instrument=instrument
+    def __init__(self,instrument,A,frequency):
         self.A=A
+        self.instrument=instrument
+        self.frequency=frequency
+        self.case=np.empty()
     
-    def case(self,note,instrument):
-        def f(t):
-            #todo esto pero en arrays!!!
-            return self.A * instrument.get_harmonic_amplitude()[1] *sin(2*pi*note.get_frequency()*instrument.get_harmonic_amplitude()[0]*(t-note.get_time()))
+    def case(self,note):
+        #hacerle todo para q array con los valores que toma la funcion de abajo....
+        #tener en cuenta el muestro /freq
+        """
+        f(t)=self.A * self.instrument.get_harmonic_amplitude()[1] *sin(2*pi*note.get_frequency()*self.instrument.get_harmonic_amplitude()[0]*(t-note.get_time()))
+
+        for t in range(muestreo)
+            append a self.case [t,f(t)]
+
+        algo asi creo ajajaj
+        """
+        self.case
+        return 
+        
     
-        return Wave(f)
+    def get_wave(self):
+        return Wave(self.case)
         
--- src/models/synthesizer_obj.py
+++ src/models/synthesizer_obj.py
@@ -2,9 +2,9 @@ class Synthesizer():
     def __init__(self,output_file):
         self.output_file=output_file
 
-    def synthesize(self,envolvente):
-        #falta muestrear la envolvente con la freq de muestreo (con array.arange creo)
-        self.output_file.load(envolvente)
+    def synthesize(self,onda):
+        
+        self.output_file.load(onda)
         #el load y eso hacerlo con scikit-sound 
         self.output_file.save()
         
