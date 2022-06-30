933071110b4f9fa7dc9d6cda38e53587b4a769e1
Santiago Pourteau <spourteau@udesa.edu.ar>
2022-06-30 12:50:57 -0300

    [add] mission_control_metalofono v1

--- src/mission_control_metalofono.py
+++ src/mission_control_metalofono.py
@@ -2,23 +2,21 @@ import argparse
 from xylophone.client import XyloClient
 from xylophone.xylo import XyloNote
 from xylophone.server.server import XyloServer
+from xylophone.server.server import MockXyloServer
 
-
-def interactuar(partitura,dispositivo):
-    #esto no funca... hay que pasarle bien los parametros a los objetos..
-    server=XyloServer()
-    server.start()
-    client=XyloClient()
+def interactuar(partitura,dispositivo=None):
     notes=[]
     with open(partitura,"r") as f:
         for line in f:
+            line = line.strip('\n').split(' ')
             notes.append(XyloNote(line[1],line[0],90))
+    client = XyloClient(host='localhost', port=8080)
     client.load(notes)
     client.play()
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description='metalofon')
+    parser = argparse.ArgumentParser(description='metalofone')
     parser.add_argument('-p', '--partitura', help='partitura')
     parser.add_argument('-d', '--dispositivo', help='dispositivo')
     
@@ -27,6 +25,7 @@ def main() -> None:
     interactuar(arg.partitura, arg.dispositivo)
 
 if __name__ == '__main__':
+    interactuar('partitura.txt')
     main()
 
 
