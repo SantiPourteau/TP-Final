9bbfe996c0b43fefb32e2c4eacd289390dfda8b8
maxividela2577 <mvidela@udesa.edu.ar>
2022-06-30 15:56:10 -0300

    add mas notas en obj partitura

--- src/models/music_sheet_obj.py
+++ src/models/music_sheet_obj.py
@@ -3,14 +3,13 @@ import models.note_obj
 class Music_Sheet():
     #music sheet va a guardar todas las notas del txt de partitura 
     def __init__(self, txt_sheet):
+        self.note=[]
         with open(txt_sheet) as f:
             for line in f:
                 time=line.split()[0]
                 note=line.split()[1]
                 duration=line.split()[2]
-                self.note=models.note_obj.Note(time,note,duration)
-
-#aca va a tener que poder recibir muchas notas. asi q el self.note va a ser una lista de Notas
+                self.note.append(models.note_obj.Note(time,note,duration))
 
     def get_note(self):
         return self.note
