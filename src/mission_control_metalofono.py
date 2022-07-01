c60fc55668b8d60ae4e8191d9656ad4d09059689
Santiago Pourteau <spourteau@udesa.edu.ar>
2022-07-01 20:57:58 -0300

    [add]

--- src/mission_control_metalofono.py
+++ src/mission_control_metalofono.py
@@ -1,9 +1,8 @@
 import argparse
 from models.music_sheet_obj import Music_Sheet
-from src.xylophone.xylophone.client import XyloClient
-from src.xylophone.xylophone.xylo import XyloNote
-from src.xylophone.xylophone.server.server import XyloServer
-from src.xylophone.xylophone.server.server import MockXyloServer
+from xylophone.xylophone.client import XyloClient
+from xylophone.xylophone.xylo import XyloNote
+
 
 
 
@@ -15,10 +14,9 @@ def interact(music_sheet,device=None):
         - sheet: Music sheet with start time, note and duration
         - device
     """
-    sheet = Music_Sheet(music_sheet)
+    sheet = Music_Sheet(music_sheet,2)
     client = XyloClient(host='10.42.0.1', port=8080) #esta bien que tire connection refused
-    print(sheet.filter_notes_metalofon(sheet))
-    client.load(sheet.filter_notes_metalofon(sheet))
+    client.load(sheet.get_note())
     client.play()
 
 def main() -> None:
@@ -31,7 +29,7 @@ def main() -> None:
     interact(arg.partitura, arg.dispositivo)
 
 if __name__ == '__main__':
-    interact()
+    interact('partitura.txt')
     main()
     
 
