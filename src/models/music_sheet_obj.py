f3d1e64c38da1cc5b16f8ff621bebe7259c39fd2
maxividela2577 <mvidela@udesa.edu.ar>
2022-06-30 23:04:20 -0300

    para partitura desordenada funca tambiennn

--- src/models/music_sheet_obj.py
+++ src/models/music_sheet_obj.py
@@ -5,29 +5,34 @@ class Music_Sheet():
     def __init__(self, txt_sheet):
         self.note=[]
         with open(txt_sheet) as f:
-            contador=0
+            
+            data=[]
             for line in f:
+                data.append(line.split())
+            data=sorted(data,key=lambda inner_list: inner_list[0])
+            contador=0
+            for elem in data:
                 if contador==0:
-                    time=line.split()[0]
-                    note=line.split()[1]
-                    duration=line.split()[2]
+                    time=elem[0]
+                    note=elem[1]
+                    duration=elem[2]
                     self.note.append(models.note_obj.Note(time,note,duration))
                 if contador>0:
-                    if line.split()[0]==time+duration:                     
-                        time=line.split()[0]
-                        note=line.split()[1]
-                        duration=line.split()[2]
+                    if elem[0]==time+duration:                     
+                        time=elem[0]
+                        note=elem[1]
+                        duration=elem[2]
                         self.note.append(models.note_obj.Note(time,note,duration))
                     else:
                         #genera nota vaciaaa
                         time=float(time)+float(duration)
                         note="None"
-                        duration=abs(float(time)-int(line.split()[0]))
+                        duration=abs(float(time)-int(elem[0]))
                         self.note.append(models.note_obj.Note(time,note,duration))
 
-                        time=line.split()[0]
-                        note=line.split()[1]
-                        duration=line.split()[2]
+                        time=elem[0]
+                        note=elem[1]
+                        duration=elem[2]
                         self.note.append(models.note_obj.Note(time,note,duration))
 
                 contador+=1
