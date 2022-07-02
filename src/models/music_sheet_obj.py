3f67226c77df354b8e5ea40ff9a18ce2ab5e1072
Santiago Pourteau <spourteau@udesa.edu.ar>
2022-07-01 21:20:13 -0300

    [add]

--- src/models/music_sheet_obj.py
+++ src/models/music_sheet_obj.py
@@ -8,12 +8,14 @@ class Music_Sheet():
         self.note=[]
         self.type = type
         with open(txt_sheet) as f: 
-            
             data=[]
             for line in f:
                 data.append(line.split()) #list of lists created
-            data=sorted(data,key=lambda inner_list: inner_list[0]) #sorted based on start time
+            data=sorted(data,key=lambda inner_list: int(inner_list[0])) #sorted based on start time
             if self.type == 1:
+                """
+                Filters the data from the Music Sheet to be used by the sinthesizer
+                """
                 contador=0
                 for elem in data: #iteration over data to create instances of note object
                     if contador==0: 
@@ -44,6 +46,9 @@ class Music_Sheet():
 
                     contador+=1
             elif self.type == 2:
+                """
+                Filters the data from the Music Sheet to be used by the Xylophone
+                """
                 accepted_notes = ['C7','C#7','Cb7','B6','Bb6','A6','A#6','Ab6','G6','G#6','Gb6','F6','F#6','E6','Eb6','D6','D#6','Db6','C6','C#6','B5','Bb5','A5','A#5','Ab5','G5','F5','E5','Eb5','D5','D#5','Db5','C5','C#5','B4','Bb4','A4','A#4','Ab4','G4','G#4']
                 list = []
                 for _ in data:
@@ -51,17 +56,15 @@ class Music_Sheet():
                         list.append([int(_[0]),_[1]])
                     if _[1] not in accepted_notes:
                         raise ValueError(f'{_[1]} is not in accepted notes')
-                list_sort = sorted(list)
                 idx = 0
-                list_aux = list_sort
+                list_aux = list
                 while idx+1 < len(list_aux):
-                    if list_sort[idx][0] == list_sort[idx+1][0] and list_sort[idx][1] == list_sort[idx+1][1]:
-                        list_sort.pop(idx+1)
-                    if list_sort[idx][0] == list_sort[idx+1][0] and list_sort[idx][1] != list_sort[idx+1][1]:
-                        list_sort[idx][0] += DELTA
+                    if list[idx][0] == list[idx+1][0] and list[idx][1] == list[idx+1][1]:
+                        list.pop(idx+1)
+                    if list[idx][0] == list[idx+1][0] and list[idx][1] != list[idx+1][1]:
+                        list[idx][0] += DELTA
                     idx += 1
-                list_final = sorted(list_sort)
-                print(list_final)
+                list_final = sorted(list)
                 for _ in list_final:
                         self.note.append(XyloNote(_[1],_[0],VELOCITY))
             else:
